c95d5f8c0d4ef2bcce16925837a78437c9990253
Siarhei Yafimenka <h1_n1@mail.ru>
2023-04-13 00:02:29 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+   let type = typeof (sampleActivity)
+   let number = Number(sampleActivity)
+   let K = 0.693 / HALF_LIFE_PERIOD
+   if (type == 'string' && number > 0 && number <= 15) {
+      let result = Math.ceil(Math.log(MODERN_ACTIVITY / (+sampleActivity)) / K)
+      return result;
+   } else { return false }
 }
 
 module.exports = {
-  dateSample
+   dateSample
 };

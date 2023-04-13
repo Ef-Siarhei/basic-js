a478cdcdb713b269abc1771e12d5899a8557316e
Siarhei Yafimenka <h1_n1@mail.ru>
2023-04-13 23:43:43 +0300

    feat: add error

--- src/what-season.js
+++ src/what-season.js
@@ -14,29 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(date) {
    // throw new NotImplementedError('Not implemented');
 
-
+   if (!(date instanceof Date)) { throw new Error('Invalid date!') }
    if (isNaN(date)) { return 'Unable to determine the time of year!' }
-   else {
-      if (Object.prototype.toString.call(date) !== "[object Date]") { return 'Invalid date!' }
+   // else {
+   // if (Object.prototype.toString.call(date) !== "[object Date]" && !isNaN(date)) { return 'Invalid date!' }
 
-      // if (!(date instanceof Date)) { return 'Invalid date!' }
-      else {
-         // try {
+   // if (!(date instanceof Date)) { return 'Invalid date!' }
+   // else {
+   // try {
 
-         let month = date.getMonth()
-         if (month == 11 || month <= 1) return "winter"
-         if (month <= 4) return "spring"
-         if (month <= 7) return "summer"
-         if (month <= 10) return "autumn"
+   let month = date.getMonth()
+   if (month == 11 || month <= 1) return "winter"
+   if (month <= 4) return "spring"
+   if (month <= 7) return "summer"
+   if (month <= 10) return "autumn"
 
-         // }
-         // catch (error) {
-         //    console.log("Invalid date!")
-         // }
+   // }
+   // catch (error) {
+   //    console.log("Invalid date!")
+   // }
 
-      }
-   }
+   // }
 }
+
 
 module.exports = {
    getSeason

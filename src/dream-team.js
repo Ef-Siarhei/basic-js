0a67e4b87ac0b242566dfec8cf668b36a195e3c3
Siarhei <h1_n1@mail.ru>
2023-04-13 11:41:06 +0300

    feat: add Команда мечты

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+   //   throw new NotImplementedError('Not implemented');
+   if (members == null) { return false }
+   if (Array.isArray(members) || members.length > 0) {
+      let result = members.filter(el => typeof el == 'string')
+         .map(str => {
+            return str.trim()[0].toUpperCase()
+         })
+      return result.sort((a, b) => a.localeCompare(b)).join('');
+   } else { return false }
 }
 
 module.exports = {
-  createDreamTeam
+   createDreamTeam
 };
